61c5fd45ca88f282d5817d30291136a0f634cde8
lurei <ludek.reif@gmail.com>
2017-06-17 12:17:54 +0200

    Reload parser

--- parse_html.py
+++ parse_html.py
@@ -1,2 +1,39 @@
+from lxml import html
+from collections import defaultdict
+import requests
+import json
 
+def from_html_to_json(url):
 
+    requests_session = requests.session()
+    page = requests_session.get(url)
+
+    html_tree = html.fromstring(page.content)
+
+    if 'www.fio.cz' in url:
+
+        structure = ('date', 'amount', 'type', 'name', 'messege', 'KS', 'VS', 'SS', 'note')
+
+        transactions = defaultdict(lambda: {'date': '',
+                                            'amount': '',
+                                            'type': '',
+                                            'name': '',
+                                            'messege': '',
+                                            'KS': '',
+                                            'VS': '',
+                                            'SS': '',
+                                            'note': ''
+                                            })
+
+        # //table/tbody/tr/td[1]/text()
+
+        for tr in enumerate(html_tree.xpath('//table/tbody/tr')[1:]):
+            for piece in enumerate(structure):
+                if piece[0] == 1:
+                    transactions[tr[0]][piece[1]] = tr[1].xpath('td[{}]/text()')
+                else:
+                    transactions[tr[0]][piece[1]] = tr[1].xpath('td[{}]/text()'.format(piece[0]+1))
+
+        return json.dumps(transactions)
+
+from_html_to_json('https://www.fio.cz/ib2/transparent?a=2600088789')
