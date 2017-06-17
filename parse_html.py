e76bf22118ca9c33fffac789be7b4fe69b105580
StefekVUT <stefanmocko.jes@gmail.com>
2017-06-17 12:07:37 +0200

    parse_xml

--- parse_html.py
+++ parse_html.py
@@ -2,9 +2,16 @@ from lxml import html
 from collections import defaultdict
 import requests
 import json
+import re
+import unicodedata
+
+
+def decode_diacritics(input_message):
+    decoded_message = unicodedata.normalize('NFD', input_message[0]).encode('ascii', 'ignore')
+    return decoded_message
+
 
 def from_html_to_csv(url):
-
     requests_session = requests.session()
     page = requests_session.get(url)
 
@@ -27,10 +34,15 @@ def from_html_to_csv(url):
 
         # //table/tbody/tr/td[1]/text()
 
+
         for tr in enumerate(html_tree.xpath('//table/tbody/tr')[1:]):
-            transactions[tr[0]]['date'] = tr[1].xpath('td[1]/text()'))
+            transactions[tr[0]]['date'] = tr[1].xpath('td[1]/text()')
+            transactions[tr[0]]['amount'] = re.findall('[0-9]+[,][0-9]+', tr[1].xpath('td[2]/text()')[0])
+            transactions[tr[0]]['type'] = decode_diacritics(tr[1].xpath('td[3]/text()'))
 
+            transactions[tr[0]]['name'] = tr[1].xpath('td[4]/text()')
 
-        return
+        return transactions
+
 
 print(from_html_to_csv('https://www.fio.cz/ib2/transparent?a=2600088789'))
